1cad5884f8c2a482ef12caac99c6d04a26f3a9c4
Tom <8162045+tbjgolden@users.noreply.github.com>
2021-08-11 01:09:09 -0500

    bundle

--- docs/_snowpack/pkg/react.js
+++
@@ -1,8 +0,0 @@
-import { r as react } from './common/index-ae389540.js';
-export { r as default } from './common/index-ae389540.js';
-
-
-
-var useMemo = react.useMemo;
-var useState = react.useState;
-export { useMemo, useState };
